c7d203d4dcb4d0ec14b27d9b3bcd342506532955
useben <bbenji6@outlook.com>
2021-08-16 16:20:02 +0000

    nouveau commit

---
+++ frontend/src/app/list-user/list-user.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { UserInterface, UserService } from '../_services/user.service';
+
+@Component({
+  selector: 'app-list-user',
+  templateUrl: './list-user.component.html',
+  styleUrls: ['./list-user.component.css']
+})
+export class ListUserComponent implements OnInit {
+
+  users?: User[];
+  currentUser?: User;
+  _message: String = "";
+  user?: UserInterface;
+  currentIndex = -1;
+
+  constructor(private userService: UserService ) {   }
+
+  deleteUser(id: number) {
+    this.userService.deleteUser(id).subscribe(msg => this._message = msg);
+    //this.user.splice(this.user.findIndex(u => u.id === id), 1);
+  }
+
+  ngOnInit(): void {
+    this.retrieveUsers();
+  }
+
+
+
+  retrieveUsers(): void{
+    this.userService.getAll()
+    .subscribe(
+      data => {
+        this.users = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      });
+  }
+
+}
---
+++ nortaria/src/main/java/tg/stage/nortaria/NortariaApplication.java
@@ -0,0 +1,13 @@
+package tg.stage.nortaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NortariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NortariaApplication.class, args);
+	}
+
+}
